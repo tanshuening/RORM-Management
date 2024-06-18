d492cfff4e809d45d5b1ed4d537b46cf89e35340
tansh <tanshuening@gmail.com>
2024-06-18 21:30:55 +0800

    Create Navigation Bar

--- app/src/main/java/com/example/rorm_management/MainActivity.kt
+++ app/src/main/java/com/example/rorm_management/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.rorm_management
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.rorm_management.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        var NavController: NavController = findNavController(R.id.fragmentContainerView)
+        var bottomnav = findViewById<BottomNavigationView>(R.id.navigationBar)
+        bottomnav.setupWithNavController(NavController)
     }
 }
---
+++ app/src/main/java/com/example/rorm_management/adapter/menuItemAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.rorm_management.adapter
+
+class menuItemAdapter {
+}
