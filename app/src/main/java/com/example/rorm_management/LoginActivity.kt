4cd58182e0dd59fa7d0b81c55c1a6929109f90a7
tansh <tanshuening@gmail.com>
2024-06-18 21:26:22 +0800

    Login Authentication

--- app/src/main/java/com/example/rorm_management/LoginActivity.kt
+++ app/src/main/java/com/example/rorm_management/LoginActivity.kt
@@ -2,18 +2,26 @@ package com.example.rorm_management
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.rorm_management.databinding.ActivityLoginBinding
 import com.example.rorm_management.databinding.ActivitySignUpBinding
+import com.example.rorm_management.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var email: String
     private lateinit var password: String
-    
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
@@ -21,12 +29,62 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+
+        binding.loginButton.setOnClickListener {
+            email = binding.email.text.toString().trim()
+            password = binding.password.text.toString().trim()
+
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Please fill all details", Toast.LENGTH_SHORT).show()
+            } else {
+                createUserAccount(email, password)
+            }
+        }
 
         binding.signUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
+    }
+
+    private fun createUserAccount(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+                updateUi(user)
+            } else {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        saveUserData()
+                        updateUi(user)
+                    } else {
+                        Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                        Log.d("Account", "createUserAccount: Authentication failed", task.exception)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        email = binding.email.text.toString().trim()
+        password = binding.password.text.toString().trim()
+
+        val user = User(email, password)
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        userId?.let {
+            database.child("user").child(it).setValue(user)
+        }
+    }
+
+    private fun updateUi(user: FirebaseUser?) {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
